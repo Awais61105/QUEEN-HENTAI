395995b38933587150aef3d5c35c1ecb1bd41a44
Awais61105 <116716708+Awais61105@users.noreply.github.com>
2024-02-21 08:35:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94767939688', 'AWAiS KiNG', true],
+  ['94767939688', 'AWAiS KiNG', true], 
   [''] 
 ] //Number of owners
 
